a332ca43b14fc648a788cdbaa4228eea23bcfd52
sandipsinh400 <zalasandipsinh3185@gmail.com>
2025-02-27 15:39:36 +0530

    added code in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000kB
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          lodash: ["lodash"]
+        }
+      }
+    }
+  }
 })
